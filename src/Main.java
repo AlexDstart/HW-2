2a24a3bb8b64abaacc433878e31e9a9980526c00
AlexWorkPC <demyanalex@gmail.com>
2022-11-14 16:55:36 +0300

    Задача 2 ок но в следующем буду дико тупить

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,21 @@ public class Main {
         System.out.println(appleJuse);
         double milk = 45.42;
         System.out.println(milk);
+        float a = 27.12f;
+        System.out.println(a);
+        long b = 987678965549L;
+        System.out.println(b);
+        double c = 2.786;
+        System.out.println(c);
+        boolean d = 2 > 3 ;
+        System.out.println(d);
+        short f = 569;
+        System.out.println(f);
+        short g = -159;
+        System.out.println(g);
+        short x = 27897;
+        System.out.println(x);
+        byte r = 67;
+        System.out.println(r);
     }
 }
