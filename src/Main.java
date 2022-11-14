2e034e2f2920b65bbcfc3be6f8171c1e5e1289c4
AlexWorkPC <demyanalex@gmail.com>
2022-11-14 16:31:46 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Задача 1 ");
+        int apples = - 1452365;
+        System.out.println(apples);
+        byte potato = 100;
+        System.out.println(potato);
+        short oranges = 20000;
+        System.out.println(oranges);
+        long boards = 90000000;
+        System.out.println(boards);
+        float appleJuse = 3.4f;
+        System.out.println(appleJuse);
+        double milk = 45.42;
+        System.out.println(milk);
+    }
+}
